6393233806d2c7102ece15161edba21fb0edd180
JooYoungHeo <jyhur900226@naver.com>
2018-09-23 19:56:14 +0900

    [Work] add description for table definitions

--- dynamo-tat.js
+++ dynamo-tat.js
@@ -14,13 +14,13 @@ let params = {
         { AttributeName: "title", KeyType: "RANGE" }  //Sort key
     ],
     AttributeDefinitions: [       
-        { AttributeName: "year", AttributeType: "N" },
-        { AttributeName: "title", AttributeType: "S" }
-    ],
+        { AttributeName: "year", AttributeType: "N" }, // N -> 숫자
+        { AttributeName: "title", AttributeType: "S" } // S -> 문자열
+    ], // BOOL (부울), B (이진수), S (날짜 문자열), type + S (집합) ex) NS (숫자집합)
     ProvisionedThroughput: {       
-        ReadCapacityUnits: 10, 
-        WriteCapacityUnits: 10
-    }
+        ReadCapacityUnits: 10, // 초당 최대 40KB strongly read & 초당 최대 80KB eventually read
+        WriteCapacityUnits: 10 // 초당 최대 10KB write
+    } // strong read -> 최신 업데이트 반영 / eventually -> 최근 작업 결과 반영 안될 수 있음
 };
 
 dynamoDb.createTable(params, (err, data) => {
